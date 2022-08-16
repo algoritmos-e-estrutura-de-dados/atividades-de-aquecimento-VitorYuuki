cb7383c03770bbc4071d38baf01bdbfbc3d04537
Vitor Yuuki Horikawa <vitor.horikawa@hotmail.com>
2022-08-16 01:15:20 -0300

    exercicios

--- exercicios/the_greatest.py
+++ exercicios/the_greatest.py
@@ -4,4 +4,8 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-maior = (a + b + math.abs() )
+maior = (a + b + abs(a - b)) / 2
+
+resultado = (maior + c + abs(maior - c)) / 2
+
+print("O maior número é: " ,resultado)
